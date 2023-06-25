8501caeadf3577b45698419d00d2e64eed2e6814
Volkan Akyel <volkan@uikielle.com>
2023-06-25 20:57:52 +0200

    audiophile- sseparate navbar from header

---
+++ components/NavbarSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full bg-dark-100">
+    <div class="max-w-screen-xl mx-auto p-8">
+      <div class="flex justify-between text-white">
+        <p>Audiophile</p>
+        <ul class="flex justify-center gap-8 uppercase">
+          <nuxt-link to="/">Home</nuxt-link>
+          <nuxt-link to="/headphones">Headphones</nuxt-link>
+          <nuxt-link to="/speakers">Speakers</nuxt-link>
+          <nuxt-link to="/earphones">Earphones</nuxt-link>
+        </ul>
+        <img src="../assets/images/card.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
