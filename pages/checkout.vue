8d53b29dd2af7133e2a8b665852fd181f0ef87e9
Volkan Akyel <volkan@uikielle.com>
2023-07-14 01:36:52 +0200

    audiophile- create checkout page

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="px-7">
     <div class="bg-white max-w-6xl mx-auto">
-      <ProductSection
-        image-position="left"
-        image-src="../../assets/images/product-yx1-earphones/desktop/image-product.jpg"
-        title="YX1 WIRELESS EARPHONES"
-        description="Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature."
-      />
+      <div class="mt-11">
+        <h1 class="uppercase mt-20 text-l">Checkout</h1>
+        <p class="uppercase text-orange">Billing details</p>
+        <div class="flex mt-5 gap-5">
+          <div class="">
+            <label class="block" for="name">Name</label>
+            <input
+              id="name"
+              class="border border-solid border-gray-300 rounded-lg p-2"
+              type="text"
+              placeholder="Alexei Ward"
+            />
+          </div>
+          <div class="">
+            <label class="block" for="name">Email Address</label>
+            <input
+              id=""
+              class="border border-solid border-gray-300 rounded-lg p-2"
+              type="email"
+              name=""
+            />
+          </div>
+          <div class="">
+            <label class="block" for="name">Phone Number</label>
+            <input
+              id=""
+              class="border border-solid border-gray-300 rounded-lg p-2"
+              type="tel"
+              name=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      <ItemsSection />
-    </div>
-    <MarketingService />
   </div>
 </template>
 <script setup lang="ts"></script>
