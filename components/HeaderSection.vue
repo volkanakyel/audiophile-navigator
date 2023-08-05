211cf8df1a203b591fc90f64f0b188007fd097ba
Volkan Akyel <volkan@uikielle.com>
2023-08-05 14:15:20 +0200

    audiophile- update redicrection into product header Section

--- components/HeaderSection.vue
+++ components/HeaderSection.vue
@@ -7,18 +7,31 @@
       <div class="w-full p-10 gap-4 items-center my-20">
         <div class="text-white flex items-center flex-col md:block">
           <p class="uppercase">New Product</p>
-          <p class="text-xl w-80">XX99 Mark II Headphones</p>
+          <p class="text-xl w-80">{{ headphoneItem.name }}</p>
           <p class="my-4 w-80 text-center md:text-left">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
-          <button class="uppercase bg-orange p-3 my-10">See Product</button>
+          <button
+            class="uppercase bg-orange p-3 my-10"
+            @click="redirectToProduct"
+          >
+            See Product
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import data from "~/data/data";
+const headphoneItem = data.find(
+  (headphone) => headphone.slug === "xx99-mark-two-headphones"
+)!;
+const redirectToProduct = async () => {
+  await navigateTo(`/${headphoneItem.category}/${headphoneItem.slug}`);
+};
+</script>
 
 <style scoped></style>
