325750c0ffaa6742faa673bfab98e235b9fef26a
Volkan Akyel <volkan@uikielle.com>
2024-03-25 02:25:09 +0100

    audiophile: feat- close cartPanel on redirection to checkout

--- components/CartViewer.vue
+++ components/CartViewer.vue
@@ -4,22 +4,14 @@
       <p class="uppercase">Cart {{ store.getCartItems.length }}</p>
       <p class="uppercase cursor-pointer" @click="clearCart">Remove All</p>
     </div>
-    <CartItem
-      v-for="(product, index) in cartItems"
-      :key="index"
-      is-summary-section
-      class="mb-6"
-      :product="product"
-    />
+    <CartItem v-for="(product, index) in cartItems" :key="index" is-summary-section class="mb-6" :product="product" />
     <div class="flex justify-between mb-8">
       <p class="uppercase">Total</p>
       <p class="uppercase">$ {{ totalPrice }}</p>
     </div>
-    <button
-      :disabled="cartItems.length === 0"
-      class="uppercase w-full bg-orange p-3 text-white"
-    >
-      <nuxt-link to="/checkout">Checkout</nuxt-link>
+    <button :disabled="cartItems.length === 0" @click="redirectToCheckout"
+      class="uppercase w-full bg-orange p-3 text-white">
+      Checkout
     </button>
   </div>
 </template>
@@ -30,6 +22,11 @@ const store = useFiltersStore();
 const cartItems = store.getCartItems;
 const totalPrice = store.getTotalPrice;
 const clearCart = () => store.clearCartContent();
+
+const redirectToCheckout = async () => {
+  store.closeCartPanel();
+  await navigateTo(`/checkout`);
+}
 </script>
 
 <style scoped></style>
