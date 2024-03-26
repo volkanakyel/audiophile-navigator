897fc5bd9771e655e8b548c34f53e1bfba31b396
Volkan Akyel <volkan@uikielle.com>
2024-03-26 04:22:22 +0100

    audiophile: Feat- add dynamic data to recommended items

--- components/RecommendationCard.vue
+++ components/RecommendationCard.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="w-1/3 flex flex-col items-center justify-center">
-    <img src="/images/items/headphone-black.jpg" alt="" />
-    <p class="mt-8">XX99 MARK I</p>
-    <button class="uppercase bg-orange p-3 my-4">See Product</button>
+    <img :src="product.image" alt="recommended product" />
+    <p class="mt-8 text-m">{{ product.name }}</p>
+    <button class="uppercase bg-orange p-4 my-4 text-white">See Product</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { SiblingsProduct } from "~/interfaces/audioProduct";
+defineProps<{
+  product: SiblingsProduct,
+}>()
+</script>
 
 <style scoped></style>
--- components/RecommendationItems.vue
+++ components/RecommendationItems.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="w-full flex justify-center gap-4 items-center my-8">
-    <RecommendationCard />
-    <RecommendationCard />
-    <RecommendationCard />
+  <h2 class="uppercase text-center mt-20 text-l font-bold">You may also like</h2>
+  <div class="w-full flex justify-center gap-8 items-center my-8">
+    <RecommendationCard v-for="(product, index) in recommendedProduct" :key="index" :product="product" />
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { SiblingsProduct } from "~/interfaces/audioProduct";
+defineProps<{
+  recommendedProduct: SiblingsProduct[],
+}>()
+</script>
 
 <style scoped></style>
